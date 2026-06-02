0ee12fe1d95e75d53f1621f0d90801c400b9c6d3
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2026-06-02 10:25:30 +0200

    made event bus manager singleton

--- Assets/02_Scripting/EventBus/Scripts/EventBusManager.cs
+++ Assets/02_Scripting/EventBus/Scripts/EventBusManager.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 public class EventBusManager : MonoBehaviour
 {
+    public static EventBusManager instance;
+
     [SerializeField] HitPlayerEvent hitPlayerEvent;
     public HitPlayerEvent HitPlayerEvent {  get { return hitPlayerEvent; } }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
 }
