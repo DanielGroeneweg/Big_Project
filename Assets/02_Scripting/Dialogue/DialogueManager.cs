f954532e5006549f0588b9e1535a684840286f83
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2026-06-03 15:17:35 +0200

    Create Dialogue System
    Dialogue system doesn't break on too little voice lines
    Dialogue system reads .txt file and seperates it into different lines
    Dialogue system displays lines and allows the player to move on when voice line has been played

---
+++ Assets/02_Scripting/Dialogue/DialogueManager.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] BetterDialogue dialogue;
+    [SerializeField] TMP_Text textBox;
+    [SerializeField] UnityEvent onCloseDialogue;
+    [SerializeField] Button nextButton;
+    [SerializeField] AudioSource audioPlayer;
+    string[] dialogueLines;
+    int lineIndex = -1;
+    private void Start()
+    {
+        dialogueLines = dialogue.ReadLines();
+    }
+    public void NextLine()
+    {
+        lineIndex++;
+
+        if (lineIndex >= dialogueLines.Length) onCloseDialogue?.Invoke();
+
+        else
+        {
+            DisplayLine(lineIndex);
+            PlayVoiceLine(lineIndex);
+        }
+    }
+    void DisplayLine(int index)
+    {
+        textBox.text = dialogueLines[index];
+    }
+    void PlayVoiceLine(int index)
+    {
+        if (index < dialogue.VoiceLines.Count)
+        {
+            AudioClip clip = dialogue.VoiceLines[index];
+            GetComponent<AudioSource>().PlayOneShot(clip);
+            nextButton.gameObject.SetActive(false);
+            StartCoroutine(EnableButtonAfterDelay(clip.length));
+        }
+    }
+    IEnumerator EnableButtonAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        nextButton.gameObject.SetActive(true);
+    }
+}
