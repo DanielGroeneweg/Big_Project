e327806995a77dc86fae17f48d933665d93be985
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2026-06-09 14:33:46 +0200

    weapons can now be aoe

--- Assets/02_Scripting/Combat/Weapon/Weapon.cs
+++ Assets/02_Scripting/Combat/Weapon/Weapon.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 using System.Collections;
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] Collider weaponCollider;
-    float damage;
-    List<Health> hitObjects = new();
+    [SerializeField] protected Collider weaponCollider;
+    [SerializeField] bool isAOE;
+    protected float damage;
+    protected List<Health> hitObjects = new();
     public void Attack(float attackDuration, float damage)
     {
         this.damage = damage;
@@ -18,42 +19,54 @@ public class Weapon : MonoBehaviour
     {
         weaponCollider.enabled = false;
     }
-    protected virtual void OnTriggerEnter(Collider other)
+    private void Start()
+    {
+        weaponCollider.enabled = false;
+    }
+    void OnTriggerEnter(Collider other)
     {
         Debug.Log("Weapon had collision!");
         Health health = other.GetComponent<Health>();
         if (health != null)
         {
+            Debug.Log("adding");
             hitObjects.Add(health);
-            StartCoroutine(ClearList());
+            StartCoroutine(HandleList());
         }
     }
-    private void Start()
-    {
-        weaponCollider.enabled = false;
-    }
-    protected virtual IEnumerator ClearList()
+    protected virtual IEnumerator HandleList()
     {
         yield return new WaitForEndOfFrame();
 
         // handle collision here
-        Health closest = null;
-        float closestDist = Mathf.Infinity;
-        foreach (Health health in hitObjects)
+        if (isAOE)
         {
-            float dist = Vector3.Distance(transform.position, health.transform.position);
-            if (dist < closestDist)
-            {
-                closest = health;
-                closestDist = dist;
-            }
-        }
+            foreach (Health health in hitObjects)
+                health.Damage(damage);
 
-        if (closest != null)
-        {
-            closest.Damage(damage);
             DisableAttack();
         }
+        else
+        {
+            Health closest = null;
+            float closestDist = Mathf.Infinity;
+            foreach (Health health in hitObjects)
+            {
+                float dist = Vector3.Distance(transform.position, health.transform.position);
+                if (dist < closestDist)
+                {
+                    closest = health;
+                    closestDist = dist;
+                }
+            }
+
+            if (closest != null)
+            {
+                closest.Damage(damage);
+                DisableAttack();
+            }
+        }
+            
 
         hitObjects.Clear();
 
