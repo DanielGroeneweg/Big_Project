d9a2140844f9c12147881a7481ace0bc4465a70c
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2026-06-09 13:37:35 +0200

    bug fixing
    fixed a bug that caused weapons to hit 'randomly' out of all objects hit by their hitbox

--- Assets/02_Scripting/Combat/Weapon/Weapon.cs
+++ Assets/02_Scripting/Combat/Weapon/Weapon.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Collider weaponCollider;
     float damage;
+    List<Health> hitObjects = new();
     public void Attack(float attackDuration, float damage)
     {
         this.damage = damage;
@@ -15,19 +18,45 @@ public class Weapon : MonoBehaviour
     {
         weaponCollider.enabled = false;
     }
-    private void OnTriggerEnter(Collider other)
+    protected virtual void OnTriggerEnter(Collider other)
     {
         Debug.Log("Weapon had collision!");
         Health health = other.GetComponent<Health>();
         if (health != null)
         {
-            Debug.Log("found health component!");
-            health.Damage(damage);
-            weaponCollider.enabled = false;
+            hitObjects.Add(health);
+            StartCoroutine(ClearList());
         }
     }
     private void Start()
     {
         weaponCollider.enabled = false;
+    }
+    protected virtual IEnumerator ClearList()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // handle collision here
+        Health closest = null;
+        float closestDist = Mathf.Infinity;
+        foreach (Health health in hitObjects)
+        {
+            float dist = Vector3.Distance(transform.position, health.transform.position);
+            if (dist < closestDist)
+            {
+                closest = health;
+                closestDist = dist;
+            }
+        }
+
+        if (closest != null)
+        {
+            closest.Damage(damage);
+            DisableAttack();
+        }
+
+        hitObjects.Clear();
+
+        StopAllCoroutines();
     }
 }
