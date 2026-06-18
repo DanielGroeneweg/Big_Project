f665ccbd2a18821d42fede33cfc1fadac61e3287
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2026-06-18 14:55:08 +0200

    blur settings

--- Assets/02_Scripting/Combat/Presenters/blurPresenter.cs
+++ Assets/02_Scripting/Combat/Presenters/blurPresenter.cs
@@ -4,8 +4,12 @@ public class blurPresenter : Presenter
 {
     [SerializeField] int minStrength;
     [SerializeField] int maxStrength;
+    float currentStam = -1;
     public override void Present(float min, float max, float current)
     {
+        if (current == currentStam) return;
+        currentStam = current;
+
         float strengthDif = maxStrength - minStrength;
         float strength = maxStrength - (current / max) * strengthDif;
 
