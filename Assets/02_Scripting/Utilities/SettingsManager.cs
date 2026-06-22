08f28c701149e2a879baece634c6fb37cdf9b30c
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2026-06-22 13:04:24 +0200

    game settings controllers
    created a scriptable object to save settings values in between scenes
    created a script that loads and saves data into and from the scriptable object into and from json
    created a script that changes values in game scene based on settings scriptable object values
    created a script to set slider values in menu scene based on settings scriptable object values

---
+++ Assets/02_Scripting/Utilities/SettingsManager.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using UnityEngine;
+[Serializable] public class SettingsData
+{
+    public float volume;
+    public float sensitivity;
+    public float brightness;
+}
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField] Settings settings;
+    string path;
+    static SettingsManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            path = Application.persistentDataPath + "/soundSettings.json";
+            Read();
+        }
+
+        else
+            Destroy(gameObject);
+    }
+    public void SetVolume(float volume)
+    {
+        settings.volume = (Mathf.Log10(volume) * 20);
+    }
+    void Save()
+    {
+        SettingsData data = new SettingsData() { volume = settings.volume, brightness = settings.brightness, sensitivity = settings.mouseSensitivity };
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(path, json);
+    }
+    void Read()
+    {
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SettingsData data = JsonUtility.FromJson<SettingsData>(json);
+
+            if (data != null)
+            {
+                settings.volume = data.volume;
+                settings.brightness = data.brightness;
+                settings.mouseSensitivity = data.sensitivity;
+            }
+
+            else
+            {
+                settings.volume = 50;
+                settings.brightness = 50;
+                settings.mouseSensitivity = 50;
+            }
+        }
+
+        else
+        {
+            settings.volume = 50;
+            settings.brightness = 50;
+            settings.mouseSensitivity = 50;
+
+            Save();
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        Save();
+    }
+}
