112495ca3e987ba376163801ed986a2f04794a8b
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2026-06-26 10:56:08 +0200

    changed brightness
    brightness now uses a custom shader pass, this way we can also add Unity UI to it

--- Assets/02_Scripting/Settings/SettingsSetter.cs
+++ Assets/02_Scripting/Settings/SettingsSetter.cs
@@ -8,34 +8,17 @@ public class SettingsSetter : MonoBehaviour
     [SerializeField] Settings settings;
     [SerializeField] AudioMixer mixer;
     [SerializeField] PlayerController playerController;
-    [SerializeField] Volume volume;
     [SerializeField] LanguageSetter languageSetter;
-    [SerializeField] float brightnessMax = -5;
-    [SerializeField] float brightnessMin = 2;
-    Exposure exposure;
+    [SerializeField] float brightnessMax = 1;
+    [SerializeField] float brightnessMin = 0;
     private void Start()
     {
-        if (volume != null)
-        {
-            foreach (VolumeComponent comp in volume.profile.components)
-            {
-                if (comp is Exposure exp)
-                {
-                    exposure = exp;
-                }
-            }
-        }
-
         if (settings != null) LoadSettings();
     }
     public void LoadSettings()
     {
-        // Make the brightness go so that 0 in settings means brightness min (2) in volume, while 100 in settings means brightness max (-5) in volume
         float brightness = brightnessMin + settings.brightness * ((brightnessMax - brightnessMin) / 100);
-        if (exposure != null)
-        {
-            exposure.fixedExposure.Override(brightness);
-        }
+        Brightness.BrightnessSettings.brightness = brightness;
 
         if (mixer != null)
         {
